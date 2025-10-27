538e6f64f6852e785a79796df98f8fa2572ca8d0
Anubhav Singh <jonsnower07@gmail.com>
2025-10-28 00:39:26 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,18 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          500: '#4361ee',
+          600: '#3a56d4',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
