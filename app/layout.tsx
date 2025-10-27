4fa24bb62451e6b7b3aef4764509ebcb8a3323bb
Anubhav Singh <jonsnower07@gmail.com>
2025-10-28 00:33:07 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,25 @@
+import './globals.css';
+import { AuthProvider } from '../context';
+import { Navbar } from '../components/UI';
 
+export const metadata = {
+  title: 'CollabSync - Find Your Code Buddies',
+  description: 'Connect with students for amazing projects',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <AuthProvider>
+          <Navbar />
+          {children}
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
