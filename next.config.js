b2d618aabb85dbf80df4a99bb56582cdfdfe420d
Anubhav Singh <jonsnower07@gmail.com>
2025-10-28 00:39:43 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  }
+}
 
+module.exports = nextConfig
